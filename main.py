a56d22d2afb85a41d3aa881802ab2a787126435a
Joseph E. Taylor <joey.t2099@gmail.com>
2025-11-04 14:56:58 -0800

    Update header comments in main.py
    Removed copyright and author information, and added a new comment header.

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 # File: main.py
 # Author: Joseph Taylor
 # Date: 11/02/2025
-# Course: CS 493 - Cloud Application Development
-# Project 3 - Implementing a REST API Using MySQL
-
-# Copyright 2022 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Implementing a REST API Using MySQL
 
 from __future__ import annotations
 
@@ -489,3 +474,4 @@ def delete_review(review_id):
 
 init_db()
 create_tables(db)
+
